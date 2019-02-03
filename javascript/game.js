ab4c9072adc4c0426299126c89867512f6696ddf
Michael Rosario <michael@elementoinc.com>
2019-02-03 14:34:50 -0500

    cleanup javascript code and more comments

--- javascript/game.js
+++ javascript/game.js
@@ -10,7 +10,8 @@
         "Back to the Future",
         "Robocop",
         "Terminator",
-        "Total Recall"
+        "Total Recall",
+        "The Matrix"
       ]
 
       var game = {
@@ -38,24 +39,24 @@
 
 
       // Choose a random word and create the ? boxes
-      // arr - the gamewords
-      // id  - the game container
       // obj - the curret game
-      function generateGame(arr,obj) {
-         
-         // randomly get a word from gameWords
-         var randomIndex = Math.floor(Math.random() * arr.length);
-         
-         obj.currentGame = arr[randomIndex];
-         
-         arr.splice(randomIndex,1); // remove item from the array
+      function generateGame(obj) {
+
+        var newGame = false; // gate for new game
+        var gameObject = obj.gameObject;
+        var currentGame = obj.currentGame;
+        
+        // if the gameObject is empty, it means that we are starting a new game
+        if(Object.values(gameObject).length === 0){
+          newGame = true; 
+        };
 
          // start the game HTML and fill in ? boxes
          var content = document.createElement("ul"); 
          
-         for(i = 0; i < obj.currentGame.length; i++){
+         for(i = 0; i < currentGame.length; i++){
 
-            if(obj.currentGame[i] == " "){
+            if(currentGame[i] == " "){
             
               var space = document.createElement('li');
               space.setAttribute('class','space');
@@ -64,13 +65,25 @@
             } else {
 
               // assign letter as keys to the object
-              obj.gameObject[obj.currentGame[i].toLowerCase()] = false;
+              if(newGame){ gameObject[currentGame[i].toLowerCase()] = false; }
               
-              // create empty '?' boxes
-              var blank = document.createElement('li');
-              blank.setAttribute('class','blank');
-              blank.textContent = "?";
-              content.appendChild(blank);
+              if(newGame === false && gameObject[currentGame[i].toLowerCase()]){ 
+
+                // display the letter
+                var answer = document.createElement('li');
+                answer.setAttribute('class','blank answer');
+                answer.textContent = currentGame[i];
+                content.appendChild(answer);
+
+              } else {
+
+                // create empty '?' boxes
+                var blank = document.createElement('li');
+                blank.setAttribute('class','blank');
+                blank.textContent = "?";
+                content.appendChild(blank);
+
+              }
 
             }
 
@@ -82,54 +95,66 @@
 
       }
 
-      // Updates the word with letters chosen by user
-      // arr - is the string with the current word
-      // id - this is the id of the game div
-      // obj - object to keep track of the game true/false 
-      function checkAnswer(obj) {
 
-        var content = document.createElement("ul"); 
-        var arr = obj.currentGame;
-        var gameObject = obj.gameObject;
+      function updateUserGuesses(obj) {
 
-        for(i = 0; i < arr.length; i++){
+        var guessContainer = document.getElementById("userGuesses");
+        // start HTML for user guessed words
+        var userGuesses = "<h5>Incorrect Guesses : You have " + obj.userLimit + " more tries!</h5><ul>";
 
-            if(arr[i] == " "){
+        for(var j = 0; j < obj.inCorrectInput.length; j++){
+          userGuesses += "<li>"+obj.inCorrectInput[j]+"</li>";
+        }
+        userMessage.innerHTML = "";
+        
+        if(obj.inCorrectInput.length){ 
 
-               var space = document.createElement('li');
-               space.setAttribute('class','space');
-               content.appendChild(space);
+          guessContainer.innerHTML = userGuesses+"</ul>"; 
 
-            } else {
-
-                // Check if the object key is set to true
-                if(gameObject[arr[i].toLowerCase()]){
-
-                  // display the letter
-                  var answer = document.createElement('li');
-                  answer.setAttribute('class','blank answer');
-                  answer.textContent = arr[i];
-                  content.appendChild(answer);
-
-                } else {
-
-                  var blank = document.createElement('li');
-                  blank.setAttribute('class','blank');
-                  blank.textContent = "?";
-                  content.appendChild(blank);
-
-                }
-            }
         }
 
-        gameContainer.innerHTML = "";
-        gameContainer.appendChild(content);
+      }
+
+      function checkWinLose(obj) {
+
+        var messageContainer = document.getElementById("userGuesses");
+        
+        // if no more false items in the object, the user wins!
+            if(Object.values(obj.gameObject).indexOf(false) === -1){
+              obj.userWins++;
+              var message = "<div class='endMessage'><h3><br>Wins : "+ obj.userWins +"<br>C O N G R A T U L A T I O N S ! ! ! <br><span><strong>(firework goes here!)</strong></span><br><span class='startNew'>Press any key to start again!</span></h3></div>";
+              messageContainer.innerHTML = ""; // clear score container
+              showMessage(message);
+              start = 0; // restart game
+            }
+
+            // if limit is now 0, the user loses
+            if(game.userLimit === 0) {
+              var message = "<div class='endMessage'><h3>SORRY YOU DON'T HAVE ANY MORE TRIES ! ! ! <br><br><span class='startNew'>Press any key to start again!</span></h3></div>";
+              showMessage(message);
+              messageContainer.innerHTML = ""; // clear score container
+              gameWords.push(obj.currentGame); // put back the word
+              start = 0; // restart game
+            }
+
+      }
+
+
+      function showMessage(msg) {
+
+        var userMessage =  document.getElementById("userMessage");
+        userMessage.innerHTML = msg;
+
+      }
+
+      function emptyMessage() {
+
+        var userMessage =  document.getElementById("userMessage");
+        userMessage.innerHTML = "";
         
       }
 
       function checkUserInput(userInput){
-
-        var guessContainer = document.getElementById("userGuesses");
 
         userKey = userInput;
 
@@ -153,51 +178,14 @@
 
             }
 
-            // start HTML for user guessed words
-            var userGuesses = "<h5>Incorrect Guesses : You have " + game.userLimit + " more tries!</h5><ul>";
+            updateUserGuesses(game); // show incorrect input
 
-            for(var j = 0; j < game.inCorrectInput.length; j++){
-              userGuesses += "<li>"+game.inCorrectInput[j]+"</li>";
-            }
-            userMessage.innerHTML = "";
-            
-            if(game.inCorrectInput.length){ guessContainer.innerHTML = userGuesses+"</ul>"; }
+            generateGame(game); // update game screen
 
-            checkAnswer(game);
+            checkWinLose(game); // check if the user wins or loses
 
-            // if no more false items in the object, the user wins!
-            if(Object.values(game.gameObject).indexOf(false) === -1){
-              game.userWins++;
-              var message = "<div class='endMessage'><h3><br>Wins : "+ game.userWins +"<br>C O N G R A T U L A T I O N S ! ! ! <br><span><strong>(firework goes here!)</strong></span><br><span class='startNew'>Press any key to start again!</span></h3></div>";
-              guessContainer.innerHTML = ""; // clear score container
-              showMessage(message);
-              start = 0; // restart game
-            }
-
-            // if limit is now 0, the user loses
-            if(game.userLimit === 0) {
-              var message = "<div class='endMessage'><h3>SORRY YOU DON'T HAVE ANY MORE TRIES ! ! ! <br><br><span class='startNew'>Press any key to start again!</span></h3></div>";
-              showMessage(message);
-              guessContainer.innerHTML = ""; // clear score container
-              gameWords.push(game.currentGame); // put back the word
-              start = 0; // restart game
-            }
           } 
 
-      }
-
-      function showMessage(msg) {
-
-        var userMessage =  document.getElementById("userMessage");
-        userMessage.innerHTML = msg;
-
-      }
-
-      function emptyMessage() {
-
-        var userMessage =  document.getElementById("userMessage");
-        userMessage.innerHTML = "";
-        
       }
 
       // check user input
@@ -215,8 +203,13 @@
           game.userInput = [];       // Array to hold all user input
           game.inCorrectInput = [];  // Array to hold all incorrect input
           game.userLimit  = 10;      // Amount of incorrect tries
-        
-          generateGame(gameWords,game);
+          
+          // Choose a random word from Game Words
+          var randomIndex = Math.floor(Math.random() * gameWords.length);
+          game.currentGame = gameWords[randomIndex];
+          gameWords.splice(randomIndex,1); // remove item from the array
+
+          generateGame(game);
           emptyMessage();
           
           start++; // update start so this section will not run unless 0
